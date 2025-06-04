66665bc12c2b509b49c0899dfa415d573b36136b
Faradayff <rafa.jerez4@gmail.com>
2025-06-04 16:45:43 +0200

    Collector of forecasts added and changes to improve scalability

---
+++ internal/collector/collector.go
@@ -0,0 +1,53 @@
+package collector
+
+import (
+	"fmt"
+	"sync"
+	"time"
+	"weather-simple-api/internal/apis"
+	"weather-simple-api/internal/models"
+)
+
+// Add here the available APIs that you want to use for fetching weather data.
+// You can add more APIs by implementing the WeatherAPIClient interface in the apis package.
+var AvailableAPIs = []apis.WeatherClient{
+	apis.OpenMeteo{},
+	apis.WeatherAPI{APIKey: "",},
+}
+
+func FetchWeatherForecast(lat, lon float32, dates[] time.Time) ([]models.WeatherForecast, error) {
+	var wg sync.WaitGroup
+	var mu = make([]sync.Mutex, len(AvailableAPIs))
+
+	forecasts := make([]models.WeatherForecast, len(AvailableAPIs))
+
+	for i, api := range AvailableAPIs {
+		forecasts[i].ApiName = api.GetClientName()
+		forecasts[i].ForecastList = make(map[time.Time]models.DailyForecast)
+		for _, date := range dates {
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				data, err := api.Fetch(lat, lon, date)
+				if err != nil {
+					fmt.Println("Error fetching from", api.GetClientName(), ":", err)
+					return
+				}
+				mu[i].Lock()
+				forecasts[i].ForecastList[date] = data
+				mu[i].Unlock()
+			}()
+		}
+	}
+
+	wg.Wait()
+
+	var total int
+	for i := range forecasts {
+		total += len(forecasts[i].ForecastList)
+	}
+	if total == 0 {
+		return []models.WeatherForecast{}, fmt.Errorf("no data fetched from any API")
+	}
+	return forecasts, nil
+}
--- internal/models/forecast.go
+++ internal/models/forecast.go
@@ -1,14 +1,16 @@
 package models
 
+import "time"
+
 type DailyForecast struct {
-    Date         string  `json:"date"`
-    MaxTempC    float64 `json:"max_temp_c"`
-    MinTempC    float64 `json:"min_temp_c"`
+	Date     string  `json:"date"`
+	MaxTempC float64 `json:"max_temp_c"`
+	MinTempC float64 `json:"min_temp_c"`
 	//Humidity    int     `json:"humidity"`
-    //Weather     string  `json:"weather"`
+	//Weather     string  `json:"weather"`
 }
 
 type WeatherForecast struct {
-    WeatherAPI1 map[string]DailyForecast `json:"weatherAPI1"`
-    WeatherAPI2 map[string]DailyForecast `json:"weatherAPI2"`
+    ApiName string `json:"apiName"`
+	ForecastList map[time.Time]DailyForecast `json:"forecastList"`
 }

8fc0d163bd84f3897b667ac29e0512e838c15ac4
Faradayff <rafa.jerez4@gmail.com>
2025-06-04 11:47:41 +0200

    Basic server and GET call

---
+++ cmd/server.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+func fetchWeatherData(lat, lon string) (map[string]any, error) {
+	data := map[string]any{
+		"lat":         lat,
+		"lon":         lon,
+		"weather":     "sunny",
+		"temperature": 27,
+	}
+	return data, nil
+}
+
+func main() {
+    http.HandleFunc("/weather", weatherHandler)
+    log.Println("Starting server on :8080")
+    if err := http.ListenAndServe(":8080", nil); err != nil {
+    }
+}
+
+func weatherHandler(w http.ResponseWriter, r *http.Request) {
+    lat := r.URL.Query().Get("lat")
+    lon := r.URL.Query().Get("lon")
+
+    if lat == "" || lon == "" {
+        http.Error(w, "Missing lat or lon query parameters", http.StatusBadRequest)
+        return
+    }
+
+    // Simulate fetching weather data
+	data, err := fetchWeatherData(lat, lon)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+
+    w.Header().Set("Content-Type", "application/json")
+    w.WriteHeader(http.StatusOK)
+    if err := json.NewEncoder(w).Encode(data); err != nil {
+        http.Error(w, "Failed to encode response", http.StatusInternalServerError)
+    }
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module weather-simple-api
+
+go 1.24.3
